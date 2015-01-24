d9dda85c2c2bcc7c2ed2e6af4358dd7b92aa461a
HollandAGM <tpbruinsma@hotmail.nl>
2015-01-24 11:22:15 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -40,7 +40,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("http://i.imgur.com/F2lXeRq.gif");
+                    API.sendChat("/me http://i.imgur.com/F2lXeRq.gif");
                 }
             }
         };
